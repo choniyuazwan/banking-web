53eb28063832f119bb9d788e6f8e0079d06fea30
choniyuazwan <choniyuazwan@gmail.com>
2019-04-27 09:13:01 +0700

    add dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,6 +10,20 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.breakpoint = window.innerWidth <= 600 ? 1 : 4; // Breakpoint observer code
   }
 
+  public breakpoint: number; // Breakpoint observer code
+
+  // tslint:disable-next-line:no-any
+  public onResize(event: any): void {
+    this.breakpoint = 4;
+    if(event.target.innerWidth <= 600) {
+      this.breakpoint = 1;
+    } else if(event.target.innerWidth <= 900) {
+      this.breakpoint = 2;
+    }
+
+    // this.breakpoint = event.target.innerWidth <= 600 ? 1 : 4;
+  }
 }
